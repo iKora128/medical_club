feb35f5917d46d09a70125c2764e68d9b7236109
長嶋大地 <nagashimadaichi@nagashimadaichinoMacBook-Pro.local>
2020-01-14 19:47:25 +0900

    do setting

--- medical_club/settings.py
+++ medical_club/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "question.apps.QuestionConfig",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'medical_club.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "medical_club",
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": "",
+        "PORT": "",
     }
 }
 
@@ -103,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
--- question/views.py
+++ question/views.py
@@ -1,5 +1,3 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-class QuestionView(TemplateView):
-    template_name = "about.html"
